9b1f85ec64708553636f3131271557a085656ac3
Sachin Roy <sachin.jr@gmail.com>
2018-10-23 18:28:02 -0400

    Customized Extent Reports

--- src/main/java/com/grapeapp/qa/pages/CompanyPage.java
+++ src/main/java/com/grapeapp/qa/pages/CompanyPage.java
@@ -1,11 +1,14 @@
 package com.grapeapp.qa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.grapeapp.qa.base.Testbase;
 
@@ -54,7 +57,8 @@ public class CompanyPage extends Testbase {
 		//driver.switchTo().defaultContent();
 		driver.switchTo().frame("mainpanel");
 		menuCompanies.click();
-		wait.untilElementIsVisible(btnNewCompany);
+		driver.switchTo().defaultContent();
+		new WebDriverWait(driver, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.name("mainpanel")));
 		btnNewCompany.click();
 		tbCompany.sendKeys(company);
 		tbPhone.sendKeys(phone);
